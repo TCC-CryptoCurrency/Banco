52e354c7aaf882d10bd1d299fca53c80f8ba219c
GregoTsunami <69878505+GregoTsunami@users.noreply.github.com>
2021-10-15 23:49:47 -0300

    Inserts atualizados
    agora ta bom

--- TCC-BD.1.0.0.sql
+++ TCC-BD.1.0.0.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE BancoTCC
 DROP DATABASE BancoTCC
 USE BancoTCC
+USE master
 SET DATEFORMAT DMY
 
 
@@ -40,6 +41,7 @@ CREATE TABLE Noticia(
 	idNoticia INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	Titulo VARCHAR(20) NOT NULL,
 	DescNot VARCHAR(50) NOT NULL,
+	DataNot DATE NOT NULL,
 )
 
 DROP TABLE Noticia
@@ -82,12 +84,14 @@ CREATE TABLE DetalheCarteira(
 	CONSTRAINT FK_Carteira FOREIGN KEY(idCarteira) REFERENCES Usuario(idCarteira)
 )
 
+--pode ignorar daqui--
 ALTER TABLE DetalheCarteira
 ADD CONSTRAINT PK_DestinoMoeda PRIMARY KEY(idCarteira, idMoeda)
 ALTER TABLE DetalheCarteira
 ADD CONSTRAINT FK_idMoeda FOREIGN KEY(idMoeda) REFERENCES Moeda(idMoeda)
 ALTER TABLE DetalheCarteira
 ADD CONSTRAINT FK_idCarteira FOREIGN KEY(idCarteira) REFERENCES Usuario(idCarteira)
+--até aqui--
 
 DROP TABLE DetalheCarteira
 
@@ -128,16 +132,16 @@ INSERT Usuario VALUES ('Oscar', 'Oscar_tiago@hotmail.com', '17.04.2003', '189265
 INSERT Usuario VALUES ('Aline', 'Aln.2002@outlook.com', '18.03.2002', '1992880672', 'Kratos15','ENASKJ', '07.06.2021')
 INSERT Usuario VALUES ('Marcia', 'Marciax@gamail.com', '05.09.2001', '10987654321', 'Batata123','FGNMCX', '07.06.2021')
 
-INSERT Moeda VALUES ('CPDCoin', 5.00, GETDATE())
-INSERT Moeda VALUES ('Quarteta', 3.00, GETDATE())
-INSERT Moeda VALUES ('Passadina', 0.10, GETDATE())
-INSERT Moeda VALUES ('Aeropoto', 5.00, GETDATE())
-INSERT Moeda VALUES ('PneuQueimado', 5000.00, GETDATE())
-INSERT Moeda VALUES ('Bitcoin', 240000.00, GETDATE())
-INSERT Moeda VALUES ('Ethereum', 16000.00, GETDATE())
-INSERT Moeda VALUES ('Dogecoin', 1.75, GETDATE())
-INSERT Moeda VALUES ('Polkadot', 22.00, GETDATE())
-INSERT Moeda VALUES ('BitCoim', 0.02, GETDATE())
+INSERT Moeda VALUES ('Cardano', 2.23, GETDATE())
+INSERT Moeda VALUES ('Binance', 475.39, GETDATE())
+INSERT Moeda VALUES ('Tether', 1.00, GETDATE())
+INSERT Moeda VALUES ('XRP', 1.13, GETDATE())
+INSERT Moeda VALUES ('Solana', 159.50, GETDATE())
+INSERT Moeda VALUES ('Bitcoin', 61340.40, GETDATE())
+INSERT Moeda VALUES ('Ethereum', 3837.80, GETDATE())
+INSERT Moeda VALUES ('Dogecoin', 0.20, GETDATE())
+INSERT Moeda VALUES ('Polkadot', 44.15, GETDATE())
+INSERT Moeda VALUES ('USD', 1.00, GETDATE())
 
 INSERT HistoricoMoeda VALUES('25.09.2021', 3.50, 1)
 INSERT HistoricoMoeda VALUES('24.09.2021', 2.20, 1)
@@ -151,14 +155,14 @@ INSERT HistoricoMoeda VALUES('12.10.2021', 1.36, 8)
 INSERT HistoricoMoeda VALUES('12.10.2021', 31.25, 9)
 INSERT HistoricoMoeda VALUES('12.10.2021', 0.03, 10)
 
-INSERT DetalheCarteira VALUES (1.12345678, 1, 1)
+INSERT DetalheCarteira VALUES (1.00, 1, 1)
 
 INSERT Transacao VALUES (0.12345678, '07.06.2021', 2, 1, 1)
 
 INSERT Tags VALUES ('Bitcoin', 'Moeda Bitcoin')
 INSERT Tags VALUES ('Queda', 'Queda de preço')
 INSERT Tags VALUES ('Em alta', 'Moedas que estão em alta')
-INSERT Tags VALUES ('Etherium', 'Moeda Etherium')
+INSERT Tags VALUES ('Ethereum', 'Moeda Etherium')
 INSERT Tags VALUES ('Dogecoin', 'Moeda Dogecoin')
 INSERT Tags VALUES ('Brasil', 'Brasil sobre Moedas')
 INSERT Tags VALUES ('Golpes', 'Golpes sobre moedas')
@@ -169,16 +173,16 @@ INSERT Tags VALUES ('China', 'China sobre moedas')
 INSERT Tags VALUES ('Elon Musk', 'Elon Musk')
 
 
-INSERT Noticia VALUES ('Queda do Bitcoin', 'Elon Musk não aceita mais bitcoin')
-INSERT Noticia VALUES ('Hiperinflação', 'Albania Atinge Hiperinflação')
-INSERT Noticia VALUES ('Nova Moeda', 'O Google diz possibilidade de criar uma moeda')
-INSERT Noticia VALUES ('Passadina cai', 'A  moeda Passadina declare falência')
-INSERT Noticia VALUES ('A Volta por Cima', 'Moeda PneuQueimado da a volta por cima e consegue manter-se de pé')
-INSERT Noticia VALUES ('Golpe de Empresa', 'Golpe atinge Evandro Teruel')
-INSERT Noticia VALUES ('Vamos Jogar?', 'Novo jogo que mexe com Ethereum faz sucesso')
-INSERT Noticia VALUES ('Precisa de Emprego?', 'Empresa procura programadores de Blockchain')
-INSERT Noticia VALUES ('Crises no Mundo', 'Crise no petróleo pode influenciar no valor das moedas')
-INSERT Noticia VALUES ('Banido da China', 'Presidente chines bani a moeda CDPcoin')
+INSERT Noticia VALUES ('Queda do Bitcoin', 'Elon Musk não aceita mais bitcoin', '13.05.2021')
+INSERT Noticia VALUES ('El Salvador', 'El Salvador aceita Bitcoin', '07.09.2021')
+INSERT Noticia VALUES ('Empiricus', 'Nova moeda de Ex-Funcionarios do Facebook', '14.07.2021')
+INSERT Noticia VALUES ('Corinthians', 'Clube Paulista anuncia nova moeda', '02.09.2021')
+INSERT Noticia VALUES ('Flamengo', 'Time de futebol anuncia nova moeda', '13.10.2021')
+INSERT Noticia VALUES ('Atlas Quantum', 'Empresa rouba milhões de Brasileiros', '19.04.2021')
+INSERT Noticia VALUES ('Round 6', 'Aumentam buscas por moedas Sul-Coreanas', '15.10.2021')
+INSERT Noticia VALUES ('Tesla rejeita moeda', 'Tesla voltará a aceitar Bitcoin como pagamento', '21.07.2021')
+INSERT Noticia VALUES ('Mineração', 'Aprenda como funciona a mineração atualmente', '26.10.2021')
+INSERT Noticia VALUES ('China sem moedas', 'China bane criptomoedas no país','24.09.2021')
 
 INSERT DetalheTagNoticia VALUES (1,1)
 INSERT DetalheTagNoticia VALUES (1,2)
